5713cbd64379df228a6a223cb0d4ac53402d9f0d
irbzdosxx9435-debug <i.rbzdosxx9435@gmail.com>
2026-06-18 15:40:08 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
 	async fetch(_request: Request, env: Env): Promise<Response> {
 		const inputs = {
-			prompt: "油画风格的全球各大城市地标,带城市名称",
+			prompt: "油画风格的富士山",
 			num_steps: 30,		  // 增加步数可以提高画质
 			guidance: 7.5,        // 调整与提示词的贴合度
 			seed: Math.floor(Math.random() * 1000) // 随机种子，每次生成都不一样
