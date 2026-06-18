d8bdd7a9e826fe876667365f141c4df22276bda6
irbzdosxx9435-debug <i.rbzdosxx9435@gmail.com>
2026-06-18 14:35:37 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
 	async fetch(_request: Request, env: Env): Promise<Response> {
 		const inputs = {
-			prompt: "cyberpunk cat",
+			prompt: "油画风格的全球各大城市地标,带城市名称",
 		} satisfies AiTextToImageInput;
 
 		const response =
